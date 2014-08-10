06c25bb113d620a1927593df87957adfdb7bf80e
animohan <anishmohan@gmail.com>
2014-08-09 19:44:09 -0700

    Simple start

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+void helloprint(void){
+	printf("Hello World");
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	helloprint();
+	return 0;
+}
+
+
+
